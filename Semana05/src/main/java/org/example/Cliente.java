f5339368cdb88d107ac3cdb35dfbe78a0291e37a
Jymmy Dextre <pcisjdex@upc.edu.pe>
2023-11-10 20:43:43 -0500

    Codigo uml2

---
+++ Semana05/src/main/java/org/example/Cliente.java
@@ -0,0 +1,10 @@
+package org.example;
+
+public class Cliente {
+    private String nombreCliente;
+
+    public Cliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+    
+}
--- Semana05/src/main/java/org/example/Persona.java
+++ Semana05/src/main/java/org/example/Persona.java
@@ -1,5 +1,9 @@
 package org.example;
 
 public class Persona {
+    //Atributo que pertenece a la mascota
+    //Asociación
+    private Mascota mascota;
+    private String dni;
 
 }
