a9c5bba9cbd9571c90bc4d50e1c83ecad2e8efd1
Jymmy Dextre <pcisjdex@upc.edu.pe>
2023-11-10 21:46:33 -0500

    Codigo uml9

--- Semana05/src/main/java/org/example/Silla.java
+++ Semana05/src/main/java/org/example/Silla.java
@@ -5,5 +5,15 @@ public class Silla {
     private int numPatas =0;
 
     public Silla() {
+        patas = new Pata[4];
+    }
+    //Metodo para agregar Pata
+    public void agregarPata(String color, float peso) {
+        patas[numPatas] = new Pata(color,peso);
+        this.numPatas++;
+    }
+
+    public int getNumPatas() {
+        return numPatas;
     }
 }
