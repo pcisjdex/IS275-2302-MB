0ff580f33df8b3b634cd57a0af169d5f4d68e8ef
Jymmy Dextre <pcisjdex@upc.edu.pe>
2023-11-10 22:48:18 -0500

    Codigo uml10

--- Semana05/src/main/java/org/example/Silla.java
+++ Semana05/src/main/java/org/example/Silla.java
@@ -3,6 +3,8 @@ public class Silla {
     private Pata[] patas;
     private int numPatas =0;
     public Silla() {
+        //En el constructor de la silla que inicializando las patas:
+        
         patas = new Pata[4];
     }
     //Metodo para agregar Pata
