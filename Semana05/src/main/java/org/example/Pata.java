92a0c1151d1dbeb9221a4fb9abaedc8e41327b29
Jymmy Dextre <pcisjdex@upc.edu.pe>
2023-11-10 21:40:19 -0500

    Codigo uml7

---
+++ Semana05/src/main/java/org/example/Pata.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Pata {
+    private String color;
+    private float peso;
+
+    public Pata(String color, float peso) {
+        this.color = color;
+        this.peso = peso;
+    }
+}
