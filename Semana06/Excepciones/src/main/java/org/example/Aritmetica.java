1b9481929b769ee37ed331b68f91317d99ece293
Jymmy Dextre <pcisjdex@upc.edu.pe>
2023-11-17 20:06:17 -0500

    Codigo de las semanas1

--- Semana06/Excepciones/src/main/java/org/example/Aritmetica.java
+++ Semana06/Excepciones/src/main/java/org/example/Aritmetica.java
@@ -3,12 +3,19 @@ package org.example;
 public class Aritmetica {
     //
     public static int numerador = 10;
-    public static int denominador = 2;
+    public static int denominador = 0;
     public static float division;
 
     public static void main(String[] args) {
         //Crear un programa que me permita realizar una división
-        division = numerador / denominador;
+        try {
+         //Capturar
+            division = numerador / denominador;
+
+        } catch (ArithmeticException ex) {
+            //Controlando
+            System.out.println("El denominador debe ser diferente de cero");
+        }
         System.out.println(division);
 
     }
