ba5a44a1df0e598d82898eb275489dee81187399
Jymmy Dextre <pcisjdex@upc.edu.pe>
2023-11-17 20:01:38 -0500

    Codigo de las semanas1

--- Semana06/Excepciones/src/main/java/org/example/Aritmetica.java
+++ Semana06/Excepciones/src/main/java/org/example/Aritmetica.java
@@ -1,7 +1,15 @@
 package org.example;
 
 public class Aritmetica {
+    //
+    public static int numerador = 10;
+    public static int denominador = 2;
+    public static float division;
+
     public static void main(String[] args) {
-        System.out.println("Hola");
+        //Crear un programa que me permita realizar una división
+        division = numerador / denominador;
+        System.out.println(division);
+
     }
 }
