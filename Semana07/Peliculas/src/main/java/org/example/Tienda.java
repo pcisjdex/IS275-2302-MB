a72d3f341b18d936bf6af9846ebb45ee80293c9a
Jymmy Dextre <pcisjdex@upc.edu.pe>
2023-11-24 19:51:28 -0500

    Codigo de las semanas6

--- Semana07/Peliculas/src/main/java/org/example/Tienda.java
+++ Semana07/Peliculas/src/main/java/org/example/Tienda.java
@@ -1,4 +1,20 @@
 package org.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tienda {
+
+    private String nombre;
+    private List<Pelicula> listaPeliculas;
+
+    public Tienda(String nombre) {
+        this.nombre = nombre;
+        this.listaPeliculas = new ArrayList<>();
+
+    }
+
+    public void agregarPelicula(Pelicula pelicula) {
+        this.listaPeliculas.add(pelicula);
+    }
 }
