ce96441e990870f6c5d2799ed15bb8927aacb803
Jymmy Dextre <pcisjdex@upc.edu.pe>
2023-11-24 20:52:19 -0500

    Codigo de las semanas6

--- Semana07/Peliculas/src/main/java/org/example/Main.java
+++ Semana07/Peliculas/src/main/java/org/example/Main.java
@@ -7,29 +7,29 @@ public class Main {
         //RPTA A, RPTA B, RPTA C
         //1ro creado una tienda
         Tienda tienda = new Tienda("BlockBuster");
-        //2do creamos la categoria
-        //Categoria cat1;
-        //Categoria cat2;
+        //2do creamos la catogoria
+        Categoria cat1=null;
+        Categoria cat2=null;
         try {
-          Categoria cat1 = new Categoria("1","Documental");
-          Categoria  cat2 = new Categoria("2","Documental");
-            Pelicula peli1 = new Pelicula(1,"Scream 5",cat1);
+            cat1 = new Categoria("1","Suspenso");
+            cat2 = new Categoria("2","Documental");
 
+        } catch(Exception ex) {
+            System.out.println(ex.getMessage());
+        }
 
-
+        //Categoria cat1 = new Categoria("1","Suspenso");
+        //Categoria cat2 = new Categoria("2","Documental");
 
         //3ero creamos la pelicula
-
+        Pelicula peli1 = new Pelicula(1,"Scream 5",cat1);
         //peli1.agregarActor("Yena Ortega",);
         peli1.agregarActor(new Actor("Yena Ortega","USA"));
         peli1.agregarActor(new Actor("Actor 2","USA"));
 
-
         //4to agregar pelicula
         tienda.agregarPelicula(peli1);
         System.out.println(peli1.toString());
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+
     }
 }
