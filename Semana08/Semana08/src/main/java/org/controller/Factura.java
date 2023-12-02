766e63f3c5b09d0e7ea76963b9ddab9b00fdc316
Jymmy Dextre <pcisjdex@upc.edu.pe>
2023-12-01 21:57:25 -0500

    Codigo de las semanas6

---
+++ Semana08/Semana08/src/main/java/org/controller/Factura.java
@@ -0,0 +1,26 @@
+package org.controller;
+
+public abstract class Factura {
+    private int id;
+    private double importe;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getImporte() {
+        return importe;
+    }
+
+    public void setImporte(double importe) {
+        this.importe = importe;
+    }
+
+    //Metodo abstracto
+    public abstract double getImporteIgv();
+
+}
