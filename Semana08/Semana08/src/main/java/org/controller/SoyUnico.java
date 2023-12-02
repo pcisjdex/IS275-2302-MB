8f4240c72d6aecec5ff7c3aef5bbcdf835bf3c89
Jymmy Dextre <pcisjdex@upc.edu.pe>
2023-12-01 20:31:41 -0500

    Codigo de las semanas6

--- Semana08/Semana08/src/main/java/org/controller/SoyUnico.java
+++ Semana08/Semana08/src/main/java/org/controller/SoyUnico.java
@@ -1,4 +1,24 @@
 package org.controller;
 
 public class SoyUnico {
+    private String nombre;
+    private static SoyUnico soyUnico;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public static SoyUnico getSoyUnico() {
+        return soyUnico;
+    }
+
+    public static void setSoyUnico(SoyUnico soyUnico) {
+        SoyUnico.soyUnico = soyUnico;
+    }
+
+    
 }
